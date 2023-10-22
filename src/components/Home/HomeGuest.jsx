d6f1a12944a497d5806566b43d0300d983d20cb4
Billy <longnd1@bravebits.vn>
2023-10-22 13:49:59 +0700

    Update button banner guest

--- src/components/Home/HomeGuest.jsx
+++ src/components/Home/HomeGuest.jsx
@@ -1,30 +1,33 @@
 import React from "react";
-import { toggleAuthModalSubs } from "../Header/Header";
+import { useNavigate } from "react-router-dom";
 
-const BannerHomeGuest = () => (
-  <div className="parent-banner">
-    <div className="banner">
-      <div className="banner-content">
-        <h1 className="content-header">
-          Thẻ ghi nhớ kỹ thuật số và các công cụ học tốt nhất
-        </h1>
+const BannerHomeGuest = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="parent-banner">
+      <div className="banner">
+        <div className="banner-content">
+          <h1 className="content-header">
+            Thẻ ghi nhớ kỹ thuật số và các công cụ học tốt nhất
+          </h1>
 
-        <p className="content-paragraph">
-          Tham gia cùng hơn 60 triệu học sinh đang sử dụng các thẻ ghi nhớ dựa
-          trên nền tảng khoa học, các bài kiểm tra thử và lời giải chuyên gia
-          của Quizlet để cải thiện điểm số và đạt được mục tiêu.
-        </p>
+          <p className="content-paragraph">
+            Tham gia cùng hơn 60 triệu học sinh đang sử dụng các thẻ ghi nhớ dựa
+            trên nền tảng khoa học, các bài kiểm tra thử và lời giải chuyên gia
+            của Quizlet để cải thiện điểm số và đạt được mục tiêu.
+          </p>
 
-        <button
-          className="cursor-pointer transition-02 remove-style-button button-register-banner"
-          onClick={() => toggleAuthModalSubs.updateState({ type: "register" })}
-        >
-          Đăng ký miễn phí
-        </button>
+          <button
+            className="cursor-pointer transition-02 remove-style-button button-register-banner"
+            onClick={() => navigate("/register")}
+          >
+            Đăng ký miễn phí
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 const PercentStudent = () => {
   return (
@@ -37,6 +40,8 @@ const PercentStudent = () => {
 };
 
 const BannerMemory = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="banner-memory">
       <div className="banner-memory-left">
@@ -48,7 +53,7 @@ const BannerMemory = () => {
         </p>
         <button
           className="remove-style-button button-register-banner cursor-pointer"
-          onClick={() => toggleAuthModalSubs.updateState({ type: "register" })}
+          onClick={() => navigate("/register")}
         >
           Bắt đầu
         </button>
